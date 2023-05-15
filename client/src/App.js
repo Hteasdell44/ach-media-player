c8e15f0733d94076d4dc883d862ac59adf190fc4
trife631 <126350708+trife631@users.noreply.github.com>
2023-05-15 10:49:29 -0400

    "initial front end commits"

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,62 @@
 import React from "react";
 import Container from "./components/Container";
+import { Button, Embed, Grid, Image, Rail, Segment } from "semantic-ui-react";
+import { Header, Icon, Image as HeaderImage } from 'semantic-ui-react';
 
-const App = () => <Container />;
+import "./App.css";
+import "./index.css";
+
+// HEADER COMPONENT
+const HeaderExampleUsersIcon = () => (
+  <div className="header-container">
+    <Header as='h2' icon textAlign='center'>
+      <Icon name='users' circular />
+      <Header.Content>Himalaya Player</Header.Content>
+    </Header>
+    <HeaderImage
+      centered
+      size='large'
+      src="./assets/images/mountain-stencil.jpg"
+    />
+  </div>
+);
+
+function App() {
+  return (
+    <div className="App">
+      {/* Display the header component */}
+      <HeaderExampleUsersIcon />
+
+      {/* Rest of your code
+      <Button>Click Here</Button>
+      <Container /> */}
+
+      {/* EMBED */}
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Embed
+          style={{ width: "300px", height: "300px", paddingTop: 0, paddingBottom: 0 }}
+          id="125292332"
+          placeholder="/images/vimeo-example.jpg"
+          source="vimeo"
+        />
+      </div>
+
+      {/* GRID RAILS */}
+      <Grid centered columns={3}>
+        <Grid.Column>
+          <Segment>
+            <Image src="/images/wireframe/paragraph.png" />
+            <Rail position="left">
+              <Segment>Left Rail Content</Segment>
+            </Rail>
+            <Rail position="right">
+              <Segment>Right Rail Content</Segment>
+            </Rail>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
+}
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "./index.css";
+import 'semantic-ui-css/semantic.min.css';
+// import "player/dist/css/player.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
