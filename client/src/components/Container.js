45666c3b42689871e200d532491e1476ec369bc0
Athena <athenawilbon1994@aol.com>
2023-05-15 12:19:17 -0400

    folders seperated

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,10 +1,12 @@
-export default function Container() {
+import React from 'react';
+import { Container as SemanticUIContainer } from 'semantic-ui-react';
 
+const Container = () => {
   return (
-    <div>
-
-      <h1> Just A Start!</h1>
-      
-    </div>
+    <SemanticUIContainer>
+      {/* Content for the container*/}
+    </SemanticUIContainer>
   );
-}
+};
+
+export default Container;
