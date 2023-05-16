c7bd9791b9dafdc712e7d4167e6c57305b087ac1
hteasdell44 <hteasdell44@gmail.com>
2023-05-16 12:49:02 -0400

    Added Read by ID, Update, And Delete Operations For Each Model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,20 +8,33 @@ const resolvers = {
       return User.find({});
     },
 
+    specificUser: async(parent, args) => {
+      return User.findOne({ _id: args.userId });
+    },
+
     allSongs: async () => {
       return Song.find({});
+    },
+
+    specificSong: async(parent, args) => {
+      return Song.findOne({ _id: args.songId });
     },
 
     allArtists: async () => {
       return Artist.find({});
     },
 
+    specificArtist: async(parent, args) => {
+      return Artist.findOne({ _id: args.artistId });
+    },
+
     allAlbums: async () => {
       return Album.find({});
     },
 
-    // Get Specific User, Song, Artist, Album
-    // Delete Specific User, Song, Artist, Album
+    specificAlbum: async(parent, args) => {
+      return Album.findOne({ _id: args.albumId });
+    },
 
   },
 
@@ -32,9 +45,51 @@ const resolvers = {
       return newUser;
     },
 
+    updateUser: async (parent, args) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: args.userId },
+
+        { 
+
+          email: args.email,
+          password: args.password
+
+        },
+        { new: true }
+      );
+      return updatedUser;
+    },
+
+    deleteUser: async (parent, args) => {
+      const deletedUser = await User.findOneAndDelete({ _id: args.userId });
+      return deletedUser;
+    },
+
     createSong: async (parent, args) => {
       const newSong = await Song.create(args);
       return newSong;
+    },
+
+    updateSong: async (parent, args) => {
+      const updatedSong = await Song.findOneAndUpdate(
+        { _id: args.songId },
+
+        { 
+          name: args.name,
+          artist: args.artist,
+          duration: args.duration,
+          genre: args.genre,
+          album: args.album
+         },
+
+        { new: true }
+      );
+      return updatedSong;
+    },
+
+    deleteSong: async (parent, args) => {
+      const deletedSong = await Song.findOneAndDelete({ _id: args.songId });
+      return deletedSong;
     },
 
     createArtist: async (parent, args) => {
@@ -42,14 +97,51 @@ const resolvers = {
       return newArtist;
     },
 
+    updateArtist: async (parent, args) => {
+      let updatedArtist = await Artist.findOneAndUpdate(
+        { _id: args.artistId },
+        { name: args.name},
+        { new: true }
+      );
+      
+      updatedArtist = await Artist.findOneAndUpdate(
+        { _id: args.artistId },
+        { $addToSet: { albums: { $each: args.albums } } },
+        { new: true }
+      );
+
+      return updatedArtist;
+    },
+
+    deleteArtist: async (parent, args) => {
+      const deletedArtist = await Artist.findOneAndDelete({ _id: args.artistId });
+      return deletedArtist;
+    },
+
     createAlbum: async (parent, args) => {
       const newAlbum = await Album.create(args);
       return newAlbum;
     },
 
+    updateAlbum: async (parent, args) => {
+      let updatedAlbum = await Album.findOneAndUpdate(
+        { _id: args.albumId },
+        { name: args.name},
+        { new: true }
+      );
+      
+      updatedAlbum = await Album.findOneAndUpdate(
+        { _id: args.albumId },
+        { $addToSet: { songs: { $each: args.songs } } },
+        { new: true }
+      );
+      return updatedAlbum;
+    },
 
-    // Update User, Song, Artist, Album by ID
-    // Delete User, Song, Artist, Album ID
+    deleteAlbum: async (parent, args) => {
+      const deletedAlbum = await Album.findOneAndDelete({ _id: args.albumId });
+      return deletedAlbum;
+    },
 
   }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,27 +18,41 @@ type Song {
 }
 
 type Artist {
+  _id: ID!
   name: String!
   albums: [String]
 }
 
 type Album {
+  _id: ID!
   name: String!
   songs: [String]
 }
 
 type Query {
   allUsers: [User]
+  specificUser(userId: ID!): User
   allSongs: [Song]
+  specificSong(songId: ID!): Song
   allArtists: [Artist]
+  specificArtist(artistId: ID!): Artist
   allAlbums: [Album]
+  specificAlbum(albumId: ID!): Album
 }
 
 type Mutation {
   createUser(email: String!, password: String!): User
+  updateUser(userId: ID!, email: String!, password: String!): User
+  deleteUser(userId: ID!): User
   createSong(name: String!, album: String, artist: String!, genre: String, duration: Int): Song
+  updateSong(songId:ID!, name: String!, album: String, artist: String!, genre: String, duration: Int): Song
+  deleteSong(songId: ID!): Song
   createArtist(name: String!, albums: [String]): Artist
+  updateArtist(artistId: ID!, name: String!, albums: [String]): Artist
+  deleteArtist(artistId: ID!): Artist
   createAlbum(name: String!, songs: [String]): Album
+  updateAlbum(albumId: ID!, name: String!, songs: [String]): Album
+  deleteAlbum(albumId: ID!): Album
 }
 `;
 
