f8bae13b1efa1d2f880b0a338f93098b7d1970a2
Athena <athenawilbon1994@aol.com>
2023-05-19 10:39:25 -0400

    fixed route

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -71,7 +71,7 @@ const Signup = () => {
         {data ? (
           <p>
             Success! You may now head{' '}
-            <Link to="/">back to the homepage.</Link>
+            <Link to="/home">back to the homepage.</Link>
           </p>
         ) : (
           <>
